811dada5cc2b2099368e57c855ec206824a9bf1d
CristiancitoUwU <cdramirez792misena.edu.co>
2020-08-15 19:59:00 +0700

    2Segundo commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*
+git init: Inicializar el repositorio
+
+git status: ver el estado de nuestro repositorio
+
+git commit --m "(comentario)": subir al repositorio
+
+:wq: salir de commit
+
+git log: ver todos los commit realizados
+
+git checkout -- : Restaurar al ultimo cambio
+
+git diff: Ver modificaciones
+
+git branch "nombre": Linea alternativa
+
+git checkout "linea alternativa": Cambiar de linea
+
+2
+*/
